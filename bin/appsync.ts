2e7722cfb2b1b050e695cee994149f98b819ed0f
Manh Pham <manh.pham@codeleap.de>
2022-10-18 02:26:35 +0700

    update stack

--- bin/appsync.ts
+++ bin/appsync.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
+import { App } from 'aws-cdk-lib';
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
 import { AppsyncStack } from '../lib/appsync-stack';
 
-const app = new cdk.App();
+const app = new App();
 new AppsyncStack(app, 'AppsyncStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
--- lib/appsync-stack.ts
+++ lib/appsync-stack.ts
@@ -1,57 +1,105 @@
-import * as cdk from '@aws-cdk/core';
-import * as appsync from '@aws-cdk/aws-appsync';
-import { MappingTemplate } from '@aws-cdk/aws-appsync';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  CfnGraphQLApi,
+  CfnGraphQLSchema,
+  CfnDataSource,
+  CfnResolver,
+  CfnApiKey,
+} from 'aws-cdk-lib/aws-appsync';
+import { CfnUserPool, ClientAttributes, OAuthScope, UserPool, UserPoolClient, UserPoolClientIdentityProvider } from 'aws-cdk-lib/aws-cognito';
+import { Construct } from 'constructs';
+import { readFileSync } from 'fs';
 
-export class AppsyncStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class AppsyncStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
+    const userpool = new UserPool(this, 'user-pool', {
+      userPoolName: 'pool-for-app-sync',
+      autoVerify: {
+        email: true,
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: true,
+        requireDigits: true,
+        requireUppercase: false,
+        requireSymbols: false,
+      },
+    });
+    const appClient = new UserPoolClient(this, 'user-client', {
+      userPoolClientName: 'appsync-client',
+      userPool: userpool,
+      authFlows: {
+        adminUserPassword: true,
+        custom: true,
+        userSrp: true,
+        userPassword: true,
+      },
+      supportedIdentityProviders: [
+        UserPoolClientIdentityProvider.COGNITO,
+      ],
+      oAuth: {
+        callbackUrls: ['app://test'],
+        logoutUrls: ['app://test'],
+        scopes: [OAuthScope.EMAIL, OAuthScope.PHONE, OAuthScope.OPENID, OAuthScope.PROFILE, OAuthScope.COGNITO_ADMIN],
+        flows: { authorizationCodeGrant: true, implicitCodeGrant: true },
+      },
+    })
     // create app sync api
-    const api = new appsync.GraphqlApi(this, 'Api', {
+    const api = new CfnGraphQLApi(this, 'Api', {
       name: 'cdk-realtime-api',
-      schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
-      authorizationConfig: {
-        defaultAuthorization: {
-          authorizationType: appsync.AuthorizationType.API_KEY,
-          apiKeyConfig: {
-            expires: cdk.Expiration.after(cdk.Duration.days(365)),
-          },
-        },
+      authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+      userPoolConfig: {
+        userPoolId: userpool.userPoolId,
+        awsRegion: Stack.of(this).region,
+        defaultAction: 'ALLOW'
       },
       xrayEnabled: true,
     });
 
+    const apiKey = new CfnApiKey(this, 'app-sync-api-key', {
+      apiId: api.attrApiId,
+    });
+    
+    const schema = new CfnGraphQLSchema(this, 'GraphSchema', {
+      apiId: api.attrApiId,
+      definition: readFileSync('graphql/schema.graphql').toString(),
+    });
+
     // print output of graphql api
-    new cdk.CfnOutput(this, 'GraphQLAPIURL', {
-      value: api.graphqlUrl,
+    new CfnOutput(this, 'GraphQLAPIURL', {
+      value: api.attrGraphQlUrl,
     });
 
     // print region
-    new cdk.CfnOutput(this, 'Stack Region', {
+    new CfnOutput(this, 'Stack Region', {
       value: this.region,
     });
 
-    // add none source
-    const nonDataSource = api.addNoneDataSource('LocalSource', {
-      name: 'LocalResolver',
-      description: 'Local resolver (Not going out of appsync)',
+    const nonDataSource = new CfnDataSource(this, 'NonDataSource', {
+      apiId: api.attrApiId,
+      name: 'NonDataSource',
+      type: 'NONE',
     });
 
-    // create local resolver
-    nonDataSource.createResolver({
+    const localResolver = new CfnResolver(this, 'LocalResolver', {
+      apiId: api.attrApiId,
+      dataSourceName: nonDataSource.attrName,
       typeName: 'Mutation',
       fieldName: 'dataChanged',
-      requestMappingTemplate: MappingTemplate.fromString(`
+      requestMappingTemplate: `
       {
         "version": "2017-02-28",
         "payload": {
          "id": "$context.arguments.input.id",
-         "category": "$context.arguments.input.category"
+         "type": "$context.arguments.input.type",
+         "datetime": $context.arguments.input.datetime,
+         "device_id": "$context.arguments.input.device_id",
          }
-      }`),
-      responseMappingTemplate: MappingTemplate.fromString(
-        `$util.toJson($context.result)`
-      ),
+      }`,
+      responseMappingTemplate: `$util.toJson($context.result)`,
     });
+
+    localResolver.addDependsOn(schema);
   }
 }
